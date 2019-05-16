57c3d20f717be8d4bb6fcb7597c75a609f19af76
SureshKumar <suresh.kumar@ameexusa.com>
2019-05-16 12:20:45 +0530

    Dependecies check

--- combine/src/main/java/com/example/combine/Combine.java
+++ combine/src/main/java/com/example/combine/Combine.java
@@ -1,11 +1,23 @@
 package com.example.combine;
 
+
+import android.widget.ImageView;
 import com.example.maths.ArithMetic;
+import com.squareup.picasso.Picasso;
 
 public class Combine {
 
     public float getValue(){
 
         return new ArithMetic().add(5f, 6f);
+    }
+
+    public void setImage(ImageView imageView){
+        Picasso.get()
+                .load("http://rohd7stg.prod.acquia-sites.com/sites/default/files/styles/icymi_image_400x200_/public/maxresdefault_33.jpg")
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.placeholder)
+                .into(imageView);
+
     }
 }
